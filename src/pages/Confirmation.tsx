39d90f6320e3f3235b0a433817de615379a29805
Akeu-Andrade <akeusantana@gmail.com>
2021-05-14 20:53:05 -0300

    feat: mix de navegação (stack e tab navigate)
    Inicio da criação da tela MyPlants

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,4 +1,4 @@
-import { useNavigation } from '@react-navigation/core';
+import { useNavigation, useRoute } from '@react-navigation/core';
 import React, { useState } from 'react';
 import {
     SafeAreaView,
@@ -11,25 +11,46 @@ import {Button} from '../components/Button';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+interface Params{
+    title: string;
+    subtitle: string;
+    buttonTitle: string;
+    icon: 'smile' | 'hug',
+    nextScreen: string;
+}
+
+const emojis ={
+    hug: '😝',
+    smile: '😀'
+}
+
 export function Confirmation(){
     const navegation = useNavigation();
+    const routes = useRoute();
+
+    const {
+        title,
+        subtitle,
+        buttonTitle,
+        icon,
+        nextScreen
+    } = routes.params as Params;
 
     function handleMoveOn(){
-        navegation.navigate('PlantSelect');
+        navegation.navigate(nextScreen);
     }
 
     return(
         <SafeAreaView style={styles.container}>
             <View style={styles.content}>
                 <Text style={styles.emoji}>
-                    😁
+                    {emojis[icon]}
                 </Text>
                 <Text style={styles.title}>
-                    Prontinho
+                    {title}
                 </Text>
                 <Text style={styles.subtitle}>
-                    Agora vamos começar a cuidar das suas
-                    plantinhas co muito cuidado.
+                    {subtitle}
                 </Text>
                 <View  style={styles.footer}>
                     <Button 
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -8,6 +8,7 @@ import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
 import { PlantSelect } from '../pages/PlantSelect';
 import { PlantSave } from '../pages/PlantSave';
+import { MyPlants } from '../pages/MyPlants';
 
 const stackRoutes = createStackNavigator();
 
@@ -40,7 +41,10 @@ const AppRoutes: React.FC = () => (
             name="PlantSave"
             component={PlantSave}
         />
-
+        <stackRoutes.Screen
+            name="MyPlants"
+            component={MyPlants}
+        />
 
     </stackRoutes.Navigator>
 )
